31c26d6b4b56e6934c9e0cc55de26709b3883979
wisestme <wyhzest@gmail.com>
2022-08-26 12:45:17 +0100

    calculate age difference

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ console.log(40 + 8 + 23 - 6);
 
 let firstName = 'Cjijioke';
 console.log(firstName);
+
+const now = 2022;
+const ageChiji = now - 1987;
+const ageGloria = now - 1994;
+
+console.log(ageChiji - ageGloria);
