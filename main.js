87a5b66876de727257004a2e95a734322037c1bb
wisestme <wyhzest@gmail.com>
2022-08-27 01:22:15 +0100

    print my details

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-const heightJohn = 1.95;
-const massJohn = 92;
-const heightMark = 1.69;
-const massMark = 78;
+// const heightJohn = 1.95;
+// const massJohn = 92;
+// const heightMark = 1.69;
+// const massMark = 78;
 
-const johnBMI = massJohn / heightJohn ** 2;
+// const johnBMI = massJohn / heightJohn ** 2;
 
-const markBMI = massMark / heightMark ** 2;
+// const markBMI = massMark / heightMark ** 2;
 
-if (johnBMI != markBMI) {
-    if (johnBMI > markBMI) {
-        console.log("John has a higher BMI than Mark");
-    } else {
-        console.log("John has a lesser BMI than Mark")
-    }
-}
+// if (johnBMI != markBMI) {
+//     if (johnBMI > markBMI) {
+//         console.log("John has a higher BMI than Mark");
+//     } else {
+//         console.log("John has a lesser BMI than Mark")
+//     }
+// }
+const firstName = 'Chiji';
+const job = 'Teacher';
+const birthYear = 1987;
+const currentYear = 2022;
+
+const chiji = `I'm ${firstName}, a ${currentYear - birthYear} year old JavaScript ${job}`;
+
+console.log(chiji);
