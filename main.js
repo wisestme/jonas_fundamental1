a4325f24e07059fe30968c0282380e32ff4b5b66
wisestme <wyhzest@gmail.com>
2022-09-11 16:54:34 +0100

    calculate tips/totals

--- main.js
+++ main.js
@@ -237,14 +237,3 @@ for (let i = 0; i < bills.length; i++) {
 }
 
 console.log(tips, totals);
-
-let sum = 0;
-function calcAverage(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i];
-    }
-    console.log(sum / arr.length);
-}
-
-calcAverage([1, 2, 3, 4]);
-calcAverage(totals);
