02062c91debbdbd74e63fc402e415cd278d1118e
wisestme <wyhzest@gmail.com>
2022-09-14 04:45:28 +0100

    set elements

--- guess_number/script.js
+++ guess_number/script.js
@@ -1,1 +1,5 @@
 'use strict';
+document.querySelector('.message').textContent = 'Start guessing ready ???';
+document.querySelector('.number').textContent = '13';
+document.querySelector('.guess').value = '7';
+document.querySelector('.score').textContent = '15';
