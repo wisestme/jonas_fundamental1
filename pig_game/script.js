eadd7e09f3ef09a6361663ee5203f5309d2e8bd2
wisestme <wyhzest@gmail.com>
2022-09-22 22:38:39 +0100

    reset current score

--- pig_game/script.js
+++ pig_game/script.js
@@ -19,12 +19,17 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
 
 // change player turn
 document.querySelector('.btn--hold').addEventListener('click', function () {
+    setCummulativeScore();
     switchCurrentPlayer();
 })
 
 
 function setDiceImageSource() {
     const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    if (diceNumber === 1) {
+        resetCurrentScore();
+        switchCurrentPlayer();
+    }
     const diceImageSource = `dice-${diceNumber}.png`;
     diceElement.src = diceImageSource;
     return diceNumber;
@@ -40,30 +45,51 @@ let activePlayer;
 
 function updateDOM() {
     allPlayers.forEach((player, index) => {
-        // console.log(player.classList.contains('player--active'));
         if (player.classList.contains('player--active')) {
             activePlayer = `player--${index}`;
         }
     });
 
     // Get current score element for active player
-    console.log(activePlayer);
     let activePlayerDOM = document.querySelector(`.${activePlayer}`);
+    console.log(activePlayerDOM.children[1].textContent);
     let currentScoreDOM = activePlayerDOM.children[2].querySelector('.current-score');
     return currentScoreDOM;
 }
 
-let currentScore = 0;
 
 function setCurrentScore() {
     let currentScoreDOM = updateDOM();
-    currentScore += setDiceImageSource();
+    let currentScore = Number(currentScoreDOM.textContent);
+    currentScore = setDiceImageSource();
+    currentScore += Number(currentScoreDOM.textContent);
     currentScoreDOM.textContent = currentScore;
+    return currentScore;
+}
+
+// initialise current
+function resetCurrentScore() {
+    let currentScoreDOM = updateDOM();
+    currentScoreDOM.textContent = 0;
+    let currentScore = Number(currentScoreDOM.textContent);
 }
 
 // switch current player
 function switchCurrentPlayer() {
+    resetCurrentScore();
+
     allPlayers.forEach(player => {
         player.classList.toggle('player--active');
     })
+
+}
+
+function setCummulativeScore() {
+    let currentScoreDOM = updateDOM();
+    let aggregateScore = currentScoreDOM.parentElement.parentElement.children[1];
+    let currentScore = Number(currentScoreDOM.textContent);;
+    let aggregateScoreValue = Number(aggregateScore.textContent);
+    aggregateScoreValue += currentScore;
+    aggregateScore.textContent = aggregateScoreValue;
+    console.log(aggregateScore)
 }
