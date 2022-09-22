c96bf41d5b1a7a9a93802c13d9e8a44d5d43f748
wisestme <wyhzest@gmail.com>
2022-09-22 13:28:40 +0100

    set image source

--- pig_game/script.js
+++ pig_game/script.js
@@ -1,7 +1,27 @@
 'use strict';
-// initialise score
+// initialise game
 const allScoreElement = document.querySelectorAll('.score');
 
 allScoreElement.forEach(scoreElement => {
     scoreElement.textContent = 0;
 })
+
+const diceElement = document.querySelector('.dice');
+
+diceElement.classList.add('hidden');
+
+// roll dice
+document.querySelector('.btn--roll').addEventListener('click', function () {
+    setDiceImageSource();
+})
+
+
+function setDiceImageSource() {
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    const diceImageSource = `dice-${diceNumber}.png`;
+    diceElement.src = diceImageSource;
+}
+
+
+
+
