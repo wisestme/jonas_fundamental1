5a45afa72940908331f9bf32c1dc7b59b30f965a
wisestme <wyhzest@gmail.com>
2022-09-22 15:51:28 +0100

    update current player score

--- pig_game/script.js
+++ pig_game/script.js
@@ -14,6 +14,7 @@ diceElement.classList.add('hidden');
 document.querySelector('.btn--roll').addEventListener('click', function () {
     setDiceImageSource();
     displayDice();
+    setCurrentScore();
 });
 
 
@@ -21,11 +22,31 @@ function setDiceImageSource() {
     const diceNumber = Math.trunc(Math.random() * 6) + 1;
     const diceImageSource = `dice-${diceNumber}.png`;
     diceElement.src = diceImageSource;
+    return diceNumber;
 }
 
 function displayDice() {
     diceElement.classList.remove('hidden');
 }
 
+// Get active player
+const allPlayers = document.querySelectorAll('.player');
+let activePlayer;
 
+allPlayers.forEach((player, index) => {
+    // console.log(player.classList.contains('player--active'));
+    if (player.classList.contains('player--active')) {
+        activePlayer = `player--${index}`;
+    }
+});
 
+// Get current score element for active player
+console.log(activePlayer);
+let activePlayerDOM = document.querySelector(`.${activePlayer}`);
+let currentScoreDOM = activePlayerDOM.children[2].querySelector('.current-score');
+let currentScore = 0;
+
+function setCurrentScore() {
+    currentScore += setDiceImageSource();
+    currentScoreDOM.textContent = currentScore;
+}
