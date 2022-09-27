73ee5646d1e309089ad0b1ff881147a5ad2e34b7
wisestme <wyhzest@gmail.com>
2022-09-27 13:24:55 +0100

    initialise aggregate score

--- pig_game_2/script.js
+++ pig_game_2/script.js
@@ -3,5 +3,13 @@
 const playerOneAggregate = document.querySelector('.score--0');
 const playerTwoAggregate = document.querySelector('.score--1');
 const dice = document.querySelector('.dice');
+const allPlayersAggregateScore = document.querySelectorAll('.score');
 
 dice.classList.add('hidden');
+
+allPlayersAggregateScore.forEach(playerAggregateScore => {
+    playerAggregateScore.textContent = 0;
+    // console.log(playerCurrentScore);
+})
+
+
