30735ff616422b9bae866bb16db64c81eee8e33c
wisestme <wyhzest@gmail.com>
2022-09-27 12:50:38 +0100

    hide dice

--- pig_game_2/script.js
+++ pig_game_2/script.js
@@ -1,1 +1,7 @@
 'use strict';
+// Select elements
+const playerOneAggregate = document.querySelector('.score--0');
+const playerTwoAggregate = document.querySelector('.score--1');
+const dice = document.querySelector('.dice');
+
+dice.classList.add('hidden');
