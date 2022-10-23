f1a1db30f2e5754cb849778b95e057f7a9054a44
wisestme <wyhzest@gmail.com>
2022-10-23 17:03:51 +0100

    make pasta

--- main.js
+++ main.js
@@ -327,8 +327,23 @@ const restaurant = {
 
     orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
         console.log(`${this.starterMenu[starterIndex]} ${this.mainMenu[mainIndex]} ${address} ${time}`);
-    }
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your delicious pasta made with ${ing1}, ${ing2}, and ${ing3}`)
+    },
+
+    // orderPizza: function (mainIngredient, ...otherIngredients) {
+    //     console.log(mainIngredient);
+    //     console.log(otherIngredients);
+    // },
 };
+
+restaurant.orderPizza('meat', 'onions', 'suya')
+
+const ingredients = [prompt(`let's make pasta ingredient 1?`), prompt('ingredient 2?'), prompt('ingredient 3?')];
+
+// restaurant.orderPasta(...ingredients);
 
 restaurant.orderDelivery({
     time: '22:30',
