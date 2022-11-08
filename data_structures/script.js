8da0479adaa76e98cbba8855529a0df51e28f460
wisestme <wyhzest@gmail.com>
2022-11-08 09:09:56 +0100

    pass object as arguement

--- data_structures/script.js
+++ data_structures/script.js
@@ -28,8 +28,20 @@ const restaurant = {
   },
   order: function (startIndex, mainIndex) {
     return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
   }
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+})
 
 const [starter, mainCourse] = restaurant.order(0, 2);
 console.log(starter, mainCourse);
