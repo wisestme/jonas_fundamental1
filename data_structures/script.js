c91798bceaba5ca0c15e4216e22bd826b0c755cc
wisestme <wyhzest@gmail.com>
2022-11-08 21:02:40 +0100

    add to main menu

--- data_structures/script.js
+++ data_structures/script.js
@@ -68,9 +68,5 @@ const { thu: thursday, fri: { open, close }, sat, mon = [] } = openingHours;
 
 console.log(open, close);
 
-const arr = [2, 8, 9];
-
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-const newArr = [1, 2, ...arr];
-console.log(badNewArr);
-console.log(newArr);
+const newMenu = [...restaurant.mainMenu, 'Kulikuli'];
+console.log(newMenu);
